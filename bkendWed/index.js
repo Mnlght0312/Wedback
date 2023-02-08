d17962033074c9c9da2c47c212ac9450030875e8
Morning <morning@mstars-lab3-07s-iMac.local>
2023-02-08 12:01:12 +0800

    Updloading image to cloudinary

--- bkendWed/index.js
+++ bkendWed/index.js
@@ -1,20 +1,23 @@
-const port = 8000;
+const port = 8080;
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 const menuRouter = require("./routes/menu.route.js");
-// const cateRouter = require("./routes/category.route.js");
-
-const fs = require("fs");
+const proRouter = require("./routes/product.route.js");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", menuRouter);
+app.use("/api", proRouter);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Welcome Rest API" });
+});
+
+app.get("", (req, res) => {
+  res.status(200).send("<h1>Hello mfckr</h1>");
 });
 
 app.listen(port, () => console.log("server is running" + port));
